e88472d64fc62d0da553a004572904732e3a897e
Johnup2350 <johnorlysteloi2838@gmail.com>
2025-11-30 01:59:52 -0300

    ajustedescr

--- javasc.js
+++ javasc.js
@@ -6,18 +6,7 @@ function motraseA() {
     let h1 = document.getElementById("title_h1_aside");
     h1.textContent = "A Descrição";
     let p = document.getElementById("parag_aside");
-    p.textContent = "É fazendo que se aprende a fazer aquilo que se deve fazer e seus frutos " +
-        "são doces.Técnico de Suporte de TI, atuando com computação e Infraestrutura de Rede. Aspirando" +
-        "crescimento na minha carreira estou, em busca de oportunidades mais pra contratação de profissional" +
-        " autônomo, atuando na área de atendimento ao usuário oferecendo suporte e comodidade, realizando " +
-        "atendimento remoto e presencial. Possuindo experiência em Instalação de câmeras, configuração de" +
-        "softwares, eletrônico “de conserto até montagem de qualquer computador”, conhecimentos em rotina" +
-        "de Backup, Itil, Experiência em Windows, Linux, Mac, Monitoramento, CSS / Html, Java, Photoshop e" +
-        "Gráfico 3D, Falo Francês fluentemente, inglês intermediário Com CNH AD-EAR, Faculdade em ciência da" +
-        "computação. Trabalhei para inúmeras " + "pessoas da comunidade local e também empresas como Farma Brasília," +
-        "Pedras do rio, Odontoclinic, Atelier da Thereza etc... em vista disso ganhei boas avaliações que consequentemente" +
-        "me ajudou a Evoluir e Melhorar. Atualmente trabalhando como Técnico de Suporte Local" +
-        " na CTC uma Grande Empresa de Tecnologia Transformadora."
+    p.textContent = "Técnico de Suporte de TI, Cursando Ciência da Computação, estou atuando com computação e Infraestrutura de Rede e Programação. Aspirando em crescimento na minha carreira estou em busca de oportunidades ofereço suporte e comodidade realizando atendimento remoto e presencial. Possuindo experiência em configuração de Hardware/softwares, eletrônico “de conserto a montagem de computador”, tenho conhecimento em rotina de Backup, Itil, Experiência em sistemas operacionais. Sei HTML/CSS/JS, Java, Python design/Gráfico e 3D, Falo Francês, inglês “intermediário” com CNH AD-EAR, Trabalhei para inúmeras pessoas da comunidade local e empresas como Farma Brasília, Pedras do rio, Odontoclinic, Atelier da Thereza, Diva salgados etc... em vista disso ganhei boas avaliações que consequentemente me ajudou a Evoluir e Melhorar. Atualmente trabalhando como Técnico de Suporte Local na CTC uma Grande Empresa de Tecnologia Transformadora."
 }
 function motraseC() {
     const aside = document.getElementById("aside_R");
@@ -33,25 +22,25 @@ document.addEventListener("DOMContentLoaded", () => {
     function animatedivimain(targetElement) {
         anime({
             targets: targetElement,
-            scale: [0.5,1],
+            scale: [0.5, 1],
             duration: 700,
             easing: 'easeInOutQuad'
         });
     }
     const faixas = document.querySelectorAll('.divimain');
     const options = {
-        root: null, 
+        root: null,
         rootMargin: '0px',
         threshold: 0.1
     };
     const observer = new IntersectionObserver((entries, options) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                animatedivimain(entry.target.querySelector('div')); 
+                animatedivimain(entry.target.querySelector('div'));
             }
         });
     }, options);
-    
+
     faixas.forEach(divimain => {
         observer.observe(divimain);
     });
@@ -81,12 +70,12 @@ function srch() {
         p.innerHTML = "please fill in the field";
         p.style.color = "red";
         p.style.fontSize = "20px";
-    }  else {
+    } else {
         let p = document.getElementById("textinput");
         p.innerHTML = "John-orly, is being developed";
         p.style.color = "white";
         p.style.fontSize = "20px";
-    }    
+    }
     function transformers() {
         let div = document.getElementById("bodypajlan");
         div.style.backgroundColor = "red";
