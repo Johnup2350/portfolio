279ebb176e4cd461b01ce79fbee64bd1b4ada1c3
John-orly <66653651+Johnup2350@users.noreply.github.com>
2025-12-02 17:58:21 -0300

    updatesiteform

---
+++ vanime.js
@@ -0,0 +1,19 @@
+function motraseA() {
+    const aside = document.getElementById("aside_R");
+    aside.classList.toggle("oculto");
+    let h1 = document.getElementById("title_h1_aside");
+    h1.textContent = "A Descrição";
+    let p = document.getElementById("parag_aside");
+    p.textContent = "Técnico de Suporte de TI, Cursando Ciência da Computação, estou atuando com computação e Infraestrutura de Rede e Programação. Aspirando em crescimento na minha carreira estou em busca de oportunidades ofereço suporte e comodidade realizando atendimento remoto e presencial. Possuindo experiência em configuração de Hardware/softwares, eletrônico “de conserto a montagem de computador”, tenho conhecimento em rotina de Backup, Itil, Experiência em sistemas operacionais. Sei HTML/CSS/JS, Java, Python design/Gráfico e 3D, Falo Francês, inglês “intermediário” com CNH AD-EAR, Trabalhei para inúmeras pessoas da comunidade local e empresas como Farma Brasília, Pedras do rio, Odontoclinic, Atelier da Thereza, Diva salgados etc... em vista disso ganhei boas avaliações que consequentemente me ajudou a Evoluir e Melhorar. Atualmente trabalhando como Técnico de Suporte Local na CTC uma Grande Empresa de Tecnologia Transformadora."
+}
+function motraseC() {
+    const aside = document.getElementById("aside_R");
+    aside.classList.toggle("oculto");
+    let h1 = document.getElementById("title_h1_aside");
+    h1.textContent = "Atualment N2 CTC";
+    let p = document.getElementById("parag_aside");
+    p.textContent = "Atendimento ao Usuário:\n " +
+        "Realizo atendimentos presenciais ou remotos para solucionar problemas técnicos reportados pelos usuários ”+” Diagnóstico e Solução de Problemas: ”+” Identificando a causa de falhas em equipamentos, softwares e redes, e aplico as soluções adequadas. Instalo e configuro softwares, hardwares e equipamentos de acordo com as necessidades dos usuários e da empresa. Realizo verificações periódicas e manutenções preventivas em equipamentos para evitar problemas futuros." +
+        "Registro detalhadamente as atividades realizadas incluindo diagnósticos soluções e tempos de execução além de elaborar relatórios de incidentes Mantenho uma comunicação clara e eficaz com os usuários equipes de suporte e fornecedores para garantir a resolução dos problemas. escalo problemas complexos ou que não podem ser resolvidos no local para níveis superiores de suporte. Mantenho-me atualizado sobre as novas tecnoçogias."
+}
+
